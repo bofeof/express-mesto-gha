407ebabce8aa67428ba5a7a0a4e883dce8dca729
Polina Che <perhaybe@gmail.com>
2022-12-29 15:45:40 +0300

    fix: \login -> \signin

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post(
-  '/login',
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
